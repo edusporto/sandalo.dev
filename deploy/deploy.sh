6231d0e33925e667bbb1d5b820e242f992212694
Eduardo Sandalo Porto <edu@sandalo.dev>
2024-03-18 14:55:48 -0300

    Fix deploy command

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DEP_HOST="ubuntu@ssh.sandalo.dev"
-DEP_PATH="/home/ubuntu/apps/caddy/"
+DEP_PATH="/home/ubuntu/apps/caddy/site/"
 
-rsync -avP 'build' "$DEP_HOST:$DEP_PATH" && \
+rsync -avP 'build/' "$DEP_HOST:$DEP_PATH" && \
 ssh "$DEP_HOST" "cd $DEP_PATH && docker-compose restart"
